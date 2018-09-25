b3ac36ed6ab210cbdb6f6e72f0da8c2425972406
followhappyq <followhappyq@gmail.com>
2018-09-25 16:13:27 +0300

    guessing game

---
+++ src/guessing-game.js
@@ -0,0 +1,27 @@
+class GuessingGame {
+  constructor() {
+    this.min = 0;
+    this.max = 0;
+    this.solutionCandidate = 0;
+  }
+
+  setRange(min, max) {
+    this.min = min;
+    this.max = max;
+  }
+
+  guess() {
+    return (this.solutionCandidate =
+      Math.ceil((this.max - this.min) / 2) + this.min);
+  }
+
+  lower() {
+    this.setRange(this.min, this.solutionCandidate);
+  }
+
+  greater() {
+    this.setRange(this.solutionCandidate, this.max);
+  }
+}
+
+module.exports = GuessingGame;
